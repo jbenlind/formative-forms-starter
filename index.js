cc5f860a8c0db16aba8448be849a295e148cf2f8
james <monaco.james18@gmail.com>
2020-11-18 17:15:01 -0600

    phase 2 done

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
+const cookieParser = require("cookie-parser");
+const csrf = require('csurf');
 const app = express();
 const port = process.env.PORT || 3000;
-
+const csrfProtection = csrf({cookie: true});
+app.use(cookieParser());
 app.set("view engine", "pug");
 
 const users = [
@@ -17,7 +19,16 @@ const users = [
 
 app.get("/", (req, res) => {
   // res.send("Hello World!");
-  res.render("index", users);
+  res.render("index", {users});
+});
+
+app.get("/create", csrfProtection, (req, res, next) => {
+  res.render("Create-Form", { title: "Create Form", csrfToken: req.csrfToken() });
+
+});
+
+app.post("/create", csrfProtection, (req, res) => {
+  res.send("csrf working");
 });
 
 
